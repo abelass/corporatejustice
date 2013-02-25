a21edc46f43f9f28cf31749628ec3853f646d303
abelass <websolutions@mychacra.net>
2013-02-25 08:55:23 +0100

    import

---
+++ squelettes/lang/enviarmail_es.php
@@ -0,0 +1,42 @@
+<?php
+
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'envoyer_article' => 'Enviar artículo',
+'envoyer' => 'Enviar',
+'reference_de' => 'la referencia de',
+'par_courriel' => 'por correo electrónico',
+'a_destination' => 'Enviar a la dirección:',
+'donnees_optionnelles' => 'Datos opcionales',
+'separe_virgule' 	=> 'separa con comas si escribes más de una',
+'sujet' 			=> 'Asunto',
+'titre_vouloir' 	=> 'título que quieres darle al mensaje',
+'nom' 				=> 'Tu nombre',
+'adresse' 			=> 'Tu dirección de correo electrónico',
+'texte' 			=> 'Puedes a ñadir un texto en el mensaje',
+'envoi'				=> 'Enviar',
+'annuler' 			=> 'Cancelar',
+'enviar_title'      => 'Enviar la referencia de este documento por email',
+'enviar_por_email'  => 'Enviar',
+'referencia'		=>	'La referencia de',
+'enviado'			=>	'ya se ha enviado a:',
+'sentimos'			=>	'Lo sentimos',
+'problema'			=>	'Ha habido un problema y no se ha podido enviar el correo :-(',
+'hola'				=>	'Hola. Quizás esta información te pueda interesar.',
+'leer'				=>	'leer más...',
+'enviado_por'		=>	'Envíado por: ',
+'direction'			=>	'pero sin dirección de correo electrónico válida',
+'podemos'			=>	'no podemos enviar el mensaje',
+'fermer'			=>	'cerrar',
+'documento'			=>	 'Hola. Quizás este documento de',
+'interesar'			=>	 'te pueda interesar.',
+'descargarte'		=>	 'Si visitas la web podrás descargarte los siguientes documentos:',
+'source'			=>	 'fuente',
+
+
+
+);
+
+
+?>
