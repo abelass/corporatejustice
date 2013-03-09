68a728ac69a124f9bd63fdc358d20d9efe85a789
abelass <websolutions@mychacra.net>
2013-03-09 12:56:47 +0100

    charset

--- config/mes_options.php
+++ config/mes_options.php
@@ -8,6 +8,6 @@ if (!defined('_DELAI_CACHE_resultats')) define('_DELAI_CACHE_resultats',600);
 $GLOBALS['quota_cache']=50;
 if (!defined('_LOGIN_TROP_COURT')) define('_LOGIN_TROP_COURT',4);
 if (!defined('_TRANCHES')) define('_TRANCHES',0);
-if (!defined('_INTERDIRE_COMPACTE_HEAD_ECRIRE')) define('_INTERDIRE_COMPACTE_HEAD_ECRIRE',true);
+mysql_query("SET NAMES UTF8 COLLATE utf8_unicode_ci") ;
 
 ?>
