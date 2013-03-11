c657e6e6d89737732bb79fc3c3548e9f1781fa24
abelass <websolutions@mychacra.net>
2013-03-11 09:22:51 +0100

    pas bon endroit

--- config/mes_options.php
+++ config/mes_options.php
@@ -8,6 +8,5 @@ if (!defined('_DELAI_CACHE_resultats')) define('_DELAI_CACHE_resultats',600);
 $GLOBALS['quota_cache']=50;
 if (!defined('_LOGIN_TROP_COURT')) define('_LOGIN_TROP_COURT',4);
 if (!defined('_TRANCHES')) define('_TRANCHES',0);
-mysql_query("SET NAMES UTF8 COLLATE utf8_unicode_ci") ;
 
 ?>
