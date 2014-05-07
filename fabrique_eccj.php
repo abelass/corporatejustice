0b93cd633b86b413913263ffe9cf7bd9288f18d6
unknown <mullrai@D02DI1146441.net1.cec.eu.int>
2014-05-07 11:57:31 +0200

    mutation en plugin

---
+++ fabrique_eccj.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ *  Fichier généré par la Fabrique de plugin v5
+ *   le 2014-05-07 09:48:05
+ *
+ *  Ce fichier de sauvegarde peut servir à recréer
+ *  votre plugin avec le plugin «Fabrique» qui a servi à le créer.
+ *
+ *  Bien évidemment, les modifications apportées ultérieurement
+ *  par vos soins dans le code de ce plugin généré
+ *  NE SERONT PAS connues du plugin «Fabrique» et ne pourront pas
+ *  être recréées par lui !
+ *
+ *  La «Fabrique» ne pourra que régénerer le code de base du plugin
+ *  avec les informations dont il dispose.
+ *
+**/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$data = array (
+  'fabrique' => 
+  array (
+    'version' => 5,
+  ),
+  'paquet' => 
+  array (
+    'nom' => 'ECCJ',
+    'slogan' => 'Thème ECCJ',
+    'description' => 'Thème ECCJ',
+    'prefixe' => 'eccj',
+    'version' => '1.0.0',
+    'auteur' => 'Rainer Müller',
+    'auteur_lien' => 'http://websimple.be/',
+    'licence' => 'GNU/GPL',
+    'categorie' => 'squelette',
+    'etat' => 'dev',
+    'compatibilite' => '[3.0.16;3.0.*]',
+    'documentation' => '',
+    'administrations' => '',
+    'schema' => '1.0.0',
+    'formulaire_config' => '',
+    'formulaire_config_titre' => '',
+    'inserer' => 
+    array (
+      'paquet' => '',
+      'administrations' => 
+      array (
+        'maj' => '',
+        'desinstallation' => '',
+        'fin' => '',
+      ),
+      'base' => 
+      array (
+        'tables' => 
+        array (
+          'fin' => '',
+        ),
+      ),
+    ),
+    'scripts' => 
+    array (
+      'pre_copie' => '',
+      'post_creation' => '',
+    ),
+    'exemples' => 'on',
+  ),
+  'objets' => 
+  array (
+  ),
+  'images' => 
+  array (
+    'paquet' => 
+    array (
+      'logo' => 
+      array (
+        0 => 
+        array (
+          'extension' => 'gif',
+          'contenu' => 'R0lGODlh1wCFAMQfAODwB6irrNTV1X2Bg/T09d7f4F1iY5OWl+nq6nN3eL7AwWhsboiMjcnKy7O1tp6houz2ZPv94P3+7+bzNuLxF+TyJu/3g/P5ovH4kur1Vfn8zej0RfX6se73dFJXWf///yH5BAEAAB8ALAAAAADXAIUAAAX/4CeOZGmeaKqubOu+cCzPdG2vhXMMhuf/HsPgoUDcjsikcslsKRg9oHTqSTiMzax2y10iDlGqeHrAds/oNPo7niYYgfiObFbb73gY4dAGMhoEKA1gPwYCeYiJih8NYW1lLQQBUQOLlpdbe30+C4cwCAM+dZgsAgEDqKkHV0sECg+pqQGAMXG2nlsFCZseDIEjERYbAMTEFRAcJQ6GTQ0BDLHRsQV6AQt9Cwq/NQIMmwe4LFIBXAWOYwckERnF7e0VFyQI20cFfLw/4SvL+B4LDTQIeOs3YBSKcbnOiak0woK7h+0mRGCiqV++SAMtejhAr4U5jUEUiANCLsvHTQu2/0GAyJIYBQ1KClwD6UHfCQK7aFbpqEKBTh8iVSBscnITrpUtW75EUhSkzRI4xSR4YOuUVJ4nGowZUPXAzClBD5JsEpUXg4ZJ01aQcAOBQotPSWT0Q82EJIVncSg0EABrgVBTAIr9UXLJ3D5YIqRdDOEGYDdVI0c2SMLBFAOCUyDICcTBCs4/BmAloeCcAcofhiqxjI/hB6SLk06kIYAKsyNupRiou6IsENQfApBxUWBKXhOqkRB4Kyao4thpG9M4HIT3jXtA4t78ChRFbiAJRp/wCWTBYB+FkWDnhQUD9LQUaij0jATBlPQtavuAhGK9qBgDZZNCcjfoh48BIwzzXv9SMMlgnxTmJeGfAeKlwAB/KCxHYAubhXWeB/jZ8BgvrlWwYFLJyGDgDw8koeFYWZAHRIU2bEhDcRa5dmJSFsywog/axaCVFDTWMOJGW9g4g3+bFLZjSz2qOEWQMPh3HEWBJQnjDQSAdNyTLGHg45RJcOeBh0sMCQQXSsYgI0EjTADmQw3G8GNN9U0B3A3CAeFaFm3CQB1KI7AzZztsfVAkCXdS6cKPCGpxZIhLBOpClzRhccGhxUwwwp+lkHlEn6FtYWZmgG7JjU5BScApMWJ+UBylKDSKxANSpKOFqLuqSgOpGh0HG5gUJIorhY+6Ic2y0VgnwqRaYLomm77OcKT/RYlRcGisH8zUYn4/ZWcCtFnc2YWlLZhpka4fbArmBiOw9l+o4QI5rhSO0jbFudXKUO9RT06Q6Is+gFprvfaWcGS+Y0rBL2Fc1hveCMPGJvAI1Dl6J002LayFudRCbMPGGrHbrraxdZDoB/KWR++/94pb7r4ho1cgwkgCU/FDGdT5wZtSoGnCnQ4IYPTRSCd9NE9HoroEyFqKvCrOJn8gwQUZyGlMBhjMFm8byKZg6xHkNgF1r1Lri3MVzrIgUJMqjO0YugFNsWgNdMe9NmF388Oe2LzaAGwvUV9UOIht7V3IAW2PUMADzIlxZQlyCy5FAqZKQR/aNt+guG5cVTWA/7r4xFX51NNmcWTVTOStQuSf12uwCKfXja8Wg0fIOeJzx+670B/UToOZ35qt5+G0woCr77GnNHTgNiz/g+5NTLF59f0KybzvIQo/g5o/OK3EYdS3nr0e28d+GuXQ22Dm7ByuAH53MhCwZ2rnwwBa+jjj5/0Mg8OTnTwwgCCZqXwugEJfBpS/F7SMfwhz2v/qdw7nvYAAX8FQCQKYPEEU4nokcJ0KHgRBhEWKUe2zXK6qJIXJjQCDKURBU850AhGq4FolBEnxRjDB+qmLdf2ZQuNEML/qeOQc8LNhCoqYQ40YpIdSIkOFCHCtDn6AOpiRn2lQo8QUkK6JvADinRLArP8yDmCIIwigP4B3lynAjwS+ocsJCjAo4Imgix4EI0goQzKNOIpJPjAAHGyBw50cUQwLoIocvthBPKKgkHoUAxCDh7N8ARIfEyPOF8O4AkfOMZIHGk0f4eKCB2Lybi+EZBtAaBdPnkCNoJSC+HhYyRfooh98qcEk+pEANNKygTPYXyz9oDeEMWwEClClPxZoA0ls0gcMsOPzgOkg2OkxkxmSjDa3WZX7eecV0XiAA3w5gwIEYAepgAMtHCjLmQ0TCLshhTxr4B9v0gBooIznPPepB0cgcAn4BKM++UlQFgxuhzGK5UALytA5noOcSBCANZsH0YbuU6JSeCMTbplD0Vj/9KMiQEAyqWBPF0lvewZgJUgLWkhpbkEAwtxbQVa60pP+YJJoUMAzaZKAY9L0EkV0aRoUEFOQFPCnPyVhL0qKhsftVCoBYCpS5QkNVvAzBwcoamgOUISpehWpCFCaAFD51bKa9axoTata18rWtrr1rXCNq1znSte62vWueM2rXvea1zhIlaEFSFpF4zqIVFhVCQkrqwPUpVG5UpEKKsWbAEVQgIoKYLBnuGRj4VqWARTNGj5AaI0mqx+hsgazLyWMYPc6EDTdw6dCmSx5CoMAv5JgebDNwkAim1f9UGoBGhxtOA7AkV+GQyBWTANg+GoCPoSNWkHST26VS0AVBNZZ/4HFAgGM9rM48CYHcdCH0RCAAAd4d462aJtI49BVx11WVnEY5+sIR5z0ztG8AdBGCCcbvLHSjjVFq8t2bWLO88qDu+WNwyxjEAqNNlhhvNMPaApQkR9g0wcJcITJ3gYEkzEpLKEg4wpP8CDewvFIHh2BmQaaWPx5wo208wEcD5Pi4IT2cmS91wK0GZQHkyAUJTHQjhOQjntw5R6Z9EMAclK8gXAFMIURDhwgN6/HGOAAD5jJU6TbAsAc4JwYlsuVweyPEbQ4YWT+clD08+P9kBlzIgAWV3KC0y5v5VnVbXOQ90MCHBVGP/QJJG8GYgTfYiwIv7Cf43xAHy8nugdibP/xwTywOfKEZR5fE+CZSdtiNseY0sikX58SYAZvnNBad/6Ajz8VYRnfFtFthnPnKMvoDzi3zz5Y8C5KEuISeGN2XF7BrUmwC/hh6sX8TWywPz2CYY+g2HH2AT2GJKI8P9LaeN6zByAMqj7dkXcj6MFZrjG5WUOY19i2MbAlfYJdTM7bYkF2OJTdaVd/wN0bdDW8f1ltB6cbyMwmwTUQOiRqmFvV1eVENDpnD0pcA93dTvenF7xf/Oy7AVAIAmDkvV9cLJuS2/62xfVt74DTYNUnQDnCtV2Cg3P54A9uAzkCyjuVC2d2mBItcsAtAvKI4JIcNzOn+etp/OHH5zYOOb//jSTxNoMK4CAvwcBLUHCjc9vqJMawJ2RS83TfHAXX+GfLeZ50kDMAC8HetMfrrfSDw3vfJke1v5/eaqXjOeIhPzifrwE/8oQD6jZvetKfElgR4PvV214ePRKmduOiUOmHTyPJ7R51pmuUD6Daddw/oPhYixzXoL4HVuC+8rvn29g9sOALr+EZZxu+ur3u+LfnPfRwFN31984z6YuO6p4qrS6s0e49WA56dpEn0IbEswcKDc1tEJcR9JNVDyB++hSwhtSO20U8Dd3z7vTJDH7/tocS+yCTFZ37PxN1ySt/8lQ/SJAZt5kAhp8eI7/5F4VgQJZnfY9EesUHFBYFX4Yd/98SeJcXGgPAGWHhZG9Ga0FAFY9REteQS6awHwuIYSKhAMMXFAyIZJJHebwnA5DkGkCTgIgzOCVgUzXGCVKAUDbVCZTlCAZwDXBmgPtQQcdFY9tQGuARWkRUMEkHhCLwWLDnJ0Oogx/IPpQXA4ElVu81AusVANSQXR8QVkhjAgkmhWMXVc7QXiUQhU5DAM6QX/bDXbLyhFCIhnETX0MEXvIFFQqgYGfIG7UlElZoNNjlACLRhHjYZ+b1hmmoDwP2VQfHXIboL2R3iIroAoW4iI7IQMn1iI/YiJI4VFD3MUaTYzWUiJWIBtITifVDZaExXWkUAKTIUJdoUeX3Vy6wGf/NUYmpmAUOMAAUBwP6wYqRkBPMBFO5JomxiD1iN0BLmAR9giZRAWdnlUybxQK/WAOwgFCRNzLr5yI9MDsipVakh2qgaGegsl1JEIJJQB61mFbZKIKc2H7LaC05gQph0QCAAVz08Be0qH3FRQL38AIj5Q/1+CkD8ATQRA3JRAQ5wQA20Q2cEFyoQGZH9TPxR4uOMzoF048v9H+9QE5Qp4wlEJAksGQHeQjJ1ANCcEYiAAtowpFVUJIzkQCCcR8/dxm8cSfPZXotwCQLRQXUAEseYko1mSXS0hmfBiPfAQS1CHWk93WvpycB5AmpKEzfEke1JgCsMV7Qt1SM0APIqB//BqAAAtAANkFuLTAkkMCDf6JaAhAUowYIDZATaIdhhZZ6++UBDxBYm3NevBgp2/VaAoAFoZCVsqJ94kGU62eUQ3J2jIBlVciLvvcLl9gnZVBb+rQ8fYEAhMZ+uUc94QdyvkSJJbALlplYtZZvqvcg32Jq21BaQldnPzhZcGeanyZNgEl5Rtkno6Fyl8h3N8FnQ9gDLVJ0mIIfnQOOY6dzUPGZQlcY7GaUrCZrcCl1OYN1xYQLcLc8zCmcpVd2G5ln5CGctMk7OAI85GEdD3Z+kuZjwElsgvd4+rBqx5lu0vkgaKKe5wheDRCVSZicSQNt11YSRZlnndU226ltT9En/0lzj+LJXwjHEOWZnPSSnta2nngHcvogHMhYiFzHK3A3ginwmtU3hI+xjwd6btG2fCdwSTJWoAyKoNOYgtK2Ah/3obNnAsgZohAKmsX5hVEwZTkBnSUXCjsmGa7JO/sJKjolaKxGd/q5oiYwENvEfi1KnilKAkNiYjPqdEJnEzHKeTL2IE4Dn/hxDy+ZWBd6nvkpo9c5O/wQNv/5aQH6pOyHKSCkm5t3AhPIEx2iKMT5bcXjoBCmnAjllZ/neY5Hpi3pAWSloWWaRz32b9wZfZUBgJNmnp3pAYKRoCMwmFDBma+HjOknqVUKo1UgDz54Rf5QmtEHc58aqEm3DUOSHv9ieEO8MyRmEBWuMQqdwwflc4nVKA8hBU2UE3fkUVwYpTuU2mwYiAAFwIMeUBe/GggYpanrWZHBo5afRpgCMKc1inj0wYuThSMzdZQLlJYLBaJV+I9iuI60dgAAYQ+9uKkYspifWRq6sjyE+RdngoKDCk91Ya8qAEhZRKy6URc/Eg44iXj4+pPkxxyogh0lgQDqsgBodIk2VSpYCmPPlg8Be5TgMYTCZAjKmAqkkRNXBn7RkB8ZkUgGQVSBhCHymArWIaEm6ZBUB7IM8F3NIg+EUAXuKJEfC27OxAnMJBbpAVrLpJG0kxEJECKgdVk1G2czsWP0YJIhi41iClIPcooznWhQU/tRpna1W3ClP5WPUsq1upS1DNUn1Cm2fEK2BVVbuIi2LeC1bruIcBu3dFu3MBACADs=',
+        ),
+      ),
+    ),
+    'objets' => 
+    array (
+    ),
+  ),
+);
+
+?>
---
+++ lang/paquet-eccj_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'eccj_description' => 'Thème ECCJ',
+	'eccj_nom' => 'ECCJ',
+	'eccj_slogan' => 'Thème ECCJ',
+);
+
+?>
